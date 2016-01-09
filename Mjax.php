b80d481d61b2322148b008474ebdb9c9ced1d60a
masiha68 <why_god88@yahoo.com>
2016-01-09 10:43:33 +0330

    Create Mjax.php

---
+++ Mjax.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace chita\mjax;
+
+/**
+ * This is just an example.
+ */
+class Mjax extends \yii\base\Widget
+{
+    public function run()
+    {
+        return "Hello!";
+    }
+}
